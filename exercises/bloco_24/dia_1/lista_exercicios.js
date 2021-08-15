27db2ec60b3fdb1af766516dfed79465eab57745
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-15 20:49:27 -0300

    exercicio 13

--- exercises/bloco_24/dia_1/lista_exercicios.js
+++ exercises/bloco_24/dia_1/lista_exercicios.js
@@ -108,7 +108,15 @@ db.movies.updateMany(
     {$unset:{budget:"", estimatedBudget:"" }}
 );
 
-
+//Exercício 13 : Para os filmes Batman ou Home Alone , atribua a 
+//imdbRating o valor 17 , caso o valor de imdbRating seja menor que 17 .
+db.movies.updateMany(
+    {$or:[
+        {title:"Batman"},
+        {title:"Home Alone"}
+    ]},
+    {$max:{imdbRating:17}}
+);
 
 
 
