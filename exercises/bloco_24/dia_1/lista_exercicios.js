189ae5649cabb7f3e1e98e2eb195a79d20d705b6
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-14 21:58:53 -0300

    Exercicio 8 - comando

--- exercises/bloco_24/dia_1/lista_exercicios.js
+++ exercises/bloco_24/dia_1/lista_exercicios.js
@@ -66,6 +66,14 @@ db.movies.updateOne(
     {$rename:{ "budget":"estimatedBudget"}}
 );
 
+//Exercício 8 : Utilize o operador $min para alterar o budget 
+//para 5 do filme Home Alone .
+db.movies.updateOne(
+    {title:'Home Alone'},
+    {$min:{budget:5}}
+);
+
+
 
 
 
