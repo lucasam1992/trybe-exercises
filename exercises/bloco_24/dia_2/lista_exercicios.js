9d9a8564823681c85062bba393e608ad0bffa2b1
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-16 19:22:10 -0300

    Exercicio 11 - adicionando array de objetos

--- exercises/bloco_24/dia_2/lista_exercicios.js
+++ exercises/bloco_24/dia_2/lista_exercicios.js
@@ -119,6 +119,40 @@ db.movies.updateOne(
   {$set:{"cast.$.character": "Marv"}}
 );
 
+//Exercício 10: Crie um array de documentos chamado cast para o filme Batman com os seguintes dados:
+db.movies.updateOne(
+  {title:"Batman"},
+  {$push:{
+    cast:{
+      $each:[
+        {
+          "character": "Batman"
+        },
+        {
+          "character": "Alfred"
+        },
+        {
+          "character": "Coringa"
+        }
+      ]
+    }
+  }}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
