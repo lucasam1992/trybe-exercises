3670611cbd48c07d81a4c987d57d9ca036d09371
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-16 20:08:14 -0300

    Exercicio 12 sort e push

--- exercises/bloco_24/dia_2/lista_exercicios.js
+++ exercises/bloco_24/dia_2/lista_exercicios.js
@@ -173,7 +173,21 @@ db.movies.updateOne(
   }
 );
 
-
+//Exercício 12: Adicione aos atores de cast do character Batman do filme Batman os 
+//valores "Michael Keaton" , "Val Kilmer" e "George Clooney" , e deixe o 
+//array em ordem alfabética.
+db.movies.updateOne(
+  {title:"Batman",
+  "cast.character":"Batman"},
+  {
+    $push:{
+      "cast.$.actor": {
+        $each:["Michael Keaton","Val Kilmer","George Clooney"],
+        $sort:1
+      }
+    }
+  }
+);
 
 
 
