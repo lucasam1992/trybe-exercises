4bf51b096ab03a33f0ff985985c9d244dfe2d078
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-16 19:02:12 -0300

    Exercicio 7: comando push

--- exercises/bloco_24/dia_2/lista_exercicios.js
+++ exercises/bloco_24/dia_2/lista_exercicios.js
@@ -80,6 +80,11 @@ db.movies.updateOne(
   {$addToSet:{category:"action"}}
 );
 
+//Exercício 7: Adicione a categoria "90's" aos filmes Batman e Home Alone .
+db.movies.updateMany(
+  {title:{$in:["Batman","Home Alone"]}},
+  {$push:{category:"90's"}}
+);
 
 
 
