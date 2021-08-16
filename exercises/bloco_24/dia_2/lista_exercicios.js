a38996202cdbf6cdae695c77c4fd3c290d665293
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-15 22:00:15 -0300

    Exercicio 2: comando each

--- exercises/bloco_24/dia_2/lista_exercicios.js
+++ exercises/bloco_24/dia_2/lista_exercicios.js
@@ -36,7 +36,18 @@ db.movies.updateOne(
     }
 );
 
-
+//Exercício 2: Utilizando o modificador $each , adicione as categorias
+// "villain" e "comic-based" ao filme Batman .
+db.movies.updateOne(
+    {title:"Batman"},
+    {$push:{
+      category:{
+        $each:[
+        "villain","comic-based"
+      ],
+      }
+    }}
+);
 
 
 
