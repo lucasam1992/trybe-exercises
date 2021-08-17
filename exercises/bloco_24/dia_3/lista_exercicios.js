eb27a3ed7769efed7a2e25e0668bf0ac2b9dbd9d
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-16 23:15:24 -0300

    Exercicio 7

--- exercises/bloco_24/dia_3/lista_exercicios.js
+++ exercises/bloco_24/dia_3/lista_exercicios.js
@@ -77,8 +77,12 @@ db.movies.find(
     {title:1,ratings:1, _id:0}
 );
   
-
-
+//EXERCICIO7:Retorne todos os filmes com ratings entre 100 e 105 , exibindo apenas os campos title e ratings .
+db.movies.find(
+    {ratings:{$elemMatch:{$gte:100,$lte:105}}},
+    {title:1,ratings:1, _id:0}
+);
+  
 
 
 
