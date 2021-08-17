8e7490d23c9bedcb2231d4ed4dbaf08190ba9b44
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-16 23:22:55 -0300

    Exercicio 8 - comando mod

--- exercises/bloco_24/dia_3/lista_exercicios.js
+++ exercises/bloco_24/dia_3/lista_exercicios.js
@@ -83,8 +83,11 @@ db.movies.find(
     {title:1,ratings:1, _id:0}
 );
   
-
-
+//EXERCICIO 8:Retorne todos os filmes com ratings entre 64 e 105 e divisíveis por 9 , exibindo apenas os campos title e ratings .
+db.movies.find(
+    {ratings:{$elemMatch:{$gte:64,$lte:105,$mod:[9,0]}}},
+    {title:1,ratings:1, _id:0}
+);
 
 
 
