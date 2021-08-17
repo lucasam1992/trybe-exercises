9009779071f36315660065a3ade8afc6bb2c5b9f
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-16 23:02:48 -0300

    Exercicio 3

--- exercises/bloco_24/dia_3/lista_exercicios.js
+++ exercises/bloco_24/dia_3/lista_exercicios.js
@@ -40,6 +40,16 @@ db.movies.find(
     imdbRating:{$gt:7}}
 );
 
+//EXERCICIO 3:Adicione um array chamado ratings ao filme Batman com os seguintes valores: [85, 100, 102, 105] .
+// Dica: lembre-se do operador $each visto no dia anterior.
+db.movies.updateOne(
+    {title:"Batman"},
+    {$push:{
+      ratings:{
+        $each:[85,100,102,105]
+      }
+    }}
+);
 
 
 
