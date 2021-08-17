187691a10b5a884b84893ba6708a7f6a334fda58
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-16 22:58:11 -0300

    Exercicio 2

--- exercises/bloco_24/dia_3/lista_exercicios.js
+++ exercises/bloco_24/dia_3/lista_exercicios.js
@@ -34,8 +34,11 @@ db.movies.find(
     {category:{$all:["action","adventure"]}}
 );
 
-
-
+//EXERCICIO 2:Agora retorne os filmes que contenham action no array category e possuem nota do IMDB maior do que 7 .
+db.movies.find(
+    {category:{$all:["action"]},
+    imdbRating:{$gt:7}}
+);
 
 
 
