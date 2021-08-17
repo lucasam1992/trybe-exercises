6255adb7b0ebc3acb2ea5ae6274ec3d8395740cc
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-16 22:42:15 -0300

    Exercicio 1 - comando all

---
+++ exercises/bloco_24/dia_3/lista_exercicios.js
@@ -0,0 +1,58 @@
+db.movies.drop();
+db.movies.insertMany([
+  {
+    title: "Batman",
+    category: [
+      "action",
+      "adventure"
+    ],
+    imdbRating: 7.7,
+    budget: 35
+  },
+  {
+    title: "Godzilla",
+    category: [
+      "action",
+      "adventure",
+      "sci-fi"
+    ],
+    imdbRating: 6.6,
+    budget: 10
+  },
+  {
+    title: "Home Alone",
+    category: [
+      "family",
+      "comedy"
+    ],
+    imdbRating: 7.4
+  }
+]);
+
+//EXERCICIO 1:Utilizando o operador $all , retorne todos os filmes que contenham action e adventure no array category .
+db.movies.find(
+    {category:{$all:["action","adventure"]}}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
