a6c146d66f8f9feebfafdc87413d656637514224
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-19 23:24:15 -0300

    Exercicio 6

--- exercises/bloco_25/dia_1/lista_exercicios.js
+++ exercises/bloco_25/dia_1/lista_exercicios.js
@@ -61,7 +61,19 @@ db.clientes.aggregate([
     }
 ]);
 
-
+//Exercício 6: Agrupe os clientes por sexo e uf . Retorne o total de 
+//clientes de cada sexo no campo total .
+db.clientes.aggregate([
+    {
+      $group:{
+        _id:{
+          sexo:"$sexo",
+          uf:"$endereco.uf"
+        },
+        total:{$sum:1}
+      }
+    }
+]);
 
 
 
