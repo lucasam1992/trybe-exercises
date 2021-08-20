1f0e97bf7e0d3868a48c1c2d4ad14f19073991b0
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-20 18:35:32 -0300

    Exercicio 8

--- exercises/bloco_25/dia_2/exercicios_lista.js
+++ exercises/bloco_25/dia_2/exercicios_lista.js
@@ -258,7 +258,30 @@ db.vendas.aggregate([
 ]);
 
 //Exercício 8 : Selecione todas as vendas do mês de Março de 2020 , com status EM SEPARACAO . Acrescente um campo chamado dataEntregaPrevista com valor igual a três dias após a data da venda. Retorne apenas os campos clienteId , dataVenda e dataEntregaPrevista .
-
+db.vendas.aggregate([
+    {
+      $match:{
+        status:"EM SEPARACAO",
+        dataVenda:{
+          $gte:ISODate('2020-03-01'),
+          $lte:ISODate('2020-03-31')
+        }
+      }
+    },
+    {
+      $addFields:{
+        dataEntregaPrevista:{$add:["$dataVenda",3*24*60*60*1000]}
+      }
+    },
+    {
+      $project:{
+        _id:0,
+        clienteId:1,
+        dataEntregaPrevista:1,
+        dataVenda:1,
+      }
+    }
+]);
 
 
 
