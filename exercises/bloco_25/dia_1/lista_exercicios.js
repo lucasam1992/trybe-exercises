271d2fbc696c4442b8c29a70e70114ac5419df13
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-19 23:58:26 -0300

    Exercicio 8

--- exercises/bloco_25/dia_1/lista_exercicios.js
+++ exercises/bloco_25/dia_1/lista_exercicios.js
@@ -99,8 +99,25 @@ db.clientes.aggregate([
 ]);
 
 
-
-
+//Exercício 8 : Descubra quais são os 5 clientes que gastaram o maior
+// valor.
+db.vendas.aggregate([
+    {
+      $match:{
+        status: {$in:["ENTREGUE","EM SEPARACAO"]}
+      }
+    },
+    {
+      $group:{
+        _id:"$clienteId",
+        valorTotal:{$sum:"$valorTotal"}
+      }
+    },
+    {
+      $sort:{valorTotal:-1}
+    },
+    {$limit:5}
+]);
 
 
 
