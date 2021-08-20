b661934e7af4c11110069da2723338730aa5c2e1
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-20 16:27:17 -0300

    Exercicios 1 e 2

---
+++ exercises/bloco_25/dia_2/exercicios_lista.js
@@ -0,0 +1,123 @@
+//Exercício 1 : Utilize uma combinação das expressões aritméticas e adicione um campo chamado idade à coleção clientes . Algumas dicas:
+db.clientes.aggregate([
+    {
+      $addFields:{
+        idade:{
+          $floor:{
+            $divide:[
+              {$subtract:["$$NOW","$dataNascimento"]},
+              {$multiply:[86400000,365]}
+            ]
+          }
+        }
+      }
+    }
+]);
+  
+//Exercício 2 : Utilizando o novo campo idade , conte quantos clientes têm entre 18 e 25 anos.
+db.clientes.aggregate([
+    {
+    $addFields:{
+        idade:{
+          $floor:{
+            $divide:[
+              {$subtract:["$$NOW","$dataNascimento"]},
+              {$multiply:[86400000,365]}
+            ]
+          }
+        }
+      }
+    },
+    {
+      $match:{
+        idade:{
+          $gte:18,
+          $lte:25
+        }
+      }
+    },
+    {
+     $count:'totalCl'
+    }
+]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
