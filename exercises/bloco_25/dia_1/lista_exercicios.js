2a62b141badea3d1f55351053d9732a638aa06d2
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-20 00:48:03 -0300

    exercicio 11

--- exercises/bloco_25/dia_1/lista_exercicios.js
+++ exercises/bloco_25/dia_1/lista_exercicios.js
@@ -172,7 +172,37 @@ db.vendas.aggregate([
    }
 ]);
 
-
+//Exercício 11 : Descubra quantos clientes compraram menos
+// de três vezes entre os meses de Janeiro de 2020 e Março
+// de 2020 .
+db.vendas.aggregate([
+    {
+      $match:{
+        dataVenda:{
+          $gte:ISODate('2020-01-01'),
+          $lte:ISODate('2020-03-31'),
+        }
+      }
+    },
+    {
+      $group:{
+        _id:"$clienteId",
+        total:{$sum:1}
+      }
+    },
+    {
+      $match:{
+        total:{$lt:3}
+      }
+    },
+    {
+      $group:{
+        _id:null,
+        clientes:{$sum:1}
+      }
+    },
+    {$project:{_id:0}}
+]);
 
 
 
