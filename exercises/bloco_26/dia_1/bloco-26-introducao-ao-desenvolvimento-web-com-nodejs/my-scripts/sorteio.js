84cfc6e2b869a70f7eaa65c44ec3bd5462ae7e42
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-24 23:03:30 -0300

    Exercicio sorteio

---
+++ exercises/bloco_26/dia_1/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/my-scripts/sorteio.js
@@ -0,0 +1,18 @@
+const readline = require('readline-sync');
+
+
+function advinhaNumero(){
+    const numCPU = 5;
+    const numUser = readline.questionInt("Tente advinhar o numero:");
+
+    if(numCPU !== numUser){
+        console.log(`Voce errou! O numero correto eh ${numCPU}`);
+    }else{
+    console.log(`Parabéns, número correto!`);
+    }
+}
+
+advinhaNumero();
+
+
+
