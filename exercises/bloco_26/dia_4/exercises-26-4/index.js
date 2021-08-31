9202bcd9c52b46461c13370104fe9c9f33444d0f
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-30 23:31:46 -0300

    Exercicio 2 metodo POST

--- exercises/bloco_26/dia_4/exercises-26-4/index.js
+++ exercises/bloco_26/dia_4/exercises-26-4/index.js
@@ -13,5 +13,18 @@ app.listen(3001, () => console.log('ouvindo na porta 3001!'));
 
 // Exercicio 1
 //Crie uma rota GET /ping
+//Sua rota deve retornar o seguinte JSON: { message: 'pong' }
+
 app.get('/ping', (_req,res) => res.json({"message": 'pong'}));
 
+//Exercicio 2
+//Crie uma rota POST /hello
+//Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
+//Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" } .
+
+app.post('/hello', (req, res) => {
+    const { name } = req.body;
+    res.status(200).json({"message":`Hello, ${name}!`});
+});
+
+//Testar no httpie => http POST :3001/hello name=lucas
