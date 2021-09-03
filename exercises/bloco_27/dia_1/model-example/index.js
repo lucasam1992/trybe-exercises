82353073bc69d5e61c2c58e1fd692323c53e2392
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-09-02 23:33:21 -0300

    Exercicios de fixacao aula bloco 27 dia 1

--- exercises/bloco_27/dia_1/model-example/index.js
+++ exercises/bloco_27/dia_1/model-example/index.js
@@ -2,16 +2,63 @@
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 //adicionar as rotas aqui dentro do index.js
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/authors', async (_req, res) => {
     const authors = await Author.getAll();
     
     res.status(200).json(authors);
-})
+});
+
+app.get('/authors/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    const author = await Author.findById(id);
+  
+    if (!author) return res.status(404).json({ message: 'Not found' });
+  
+    res.status(200).json(author);
+});
+
+app.get('/books', async (req, res) => {
+  //  const books = await Book.getAll();
+
+    const { author_id } = req.query;
+
+    const books = (author_id) ? await Book.getByAuthorId(author_id) : await Book.getAllBooks();
+    
+    res.status(200).json(books);
+});
+
+app.get('/books/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    const book = await Book.getById(id);
+  
+    if (!book) return res.status(404).json({ message: 'Book not found' })
+  
+    res.status(200).json(book);
+});
+
+app.post('/authors', async (req, res) => {
+    const { first_name, middle_name, last_name } = req.body;
+
+    if (!Author.isValid(first_name, middle_name, last_name)) {
+        return res.status(400).json({ message: 'Dados inválidos' });
+    }
+
+    await Author.createAuthor(first_name, middle_name, last_name);
+
+    res.status(201).json({ message: 'Autor criado com sucesso! '});
+});
 
 const PORT = process.env.PORT || 3000;
 
---
+++ exercises/bloco_27/dia_1/model-example/models/Book.js
@@ -0,0 +1,59 @@
+const connection = require('./connection');
+
+const getNewBook = (bookData) => {
+    const { id, title, author_id } = bookData;
+
+    return {
+        id,
+        title,
+        author_id,
+    };
+};
+
+//converter o nome dos campos de snake_case para camelCase
+
+const serialize = (bookData) => ({
+    id: bookData.id,
+    title: bookData.title,
+    authorId: bookData.author_id
+});
+
+// busca todos os livros do banco
+
+const getAllBooks = async () => {
+    const [books] = await connection.execute(
+        'SELECT * FROM model_example.books;',
+    );
+    return books.map(serialize).map(getNewBook);
+};
+
+const getByAuthorId = async (authorId) => {
+    const query = 'SELECT * FROM model_example.books WHERE author_id=?;'
+    const [books] = await connection.execute(query, [authorId]);
+
+    return books.map(({id, title, author_id}) => ({
+        id,
+        title,
+        authorId: author_id,
+    }));
+};
+
+const getById = async (id) => {
+    const query = 'SELECT * FROM model_example.books WHERE id =?;'
+
+    const [books] = await connection.execute(query,[id]);
+
+    if(books.length ===0) return null;
+
+    return books.map(({id, title, author_id}) =>({
+        id,
+        title,
+        authorId:author_id,
+    }))[0];
+};
+
+module.exports = {
+    getAllBooks,
+    getByAuthorId,
+    getById,
+};
