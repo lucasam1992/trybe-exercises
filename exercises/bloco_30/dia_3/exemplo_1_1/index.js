99fbb9724d472e577c70c6a46671d0f12ebe9ae6
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-10-13 14:09:22 -0300

    Exemplo de relacionamento N:N

--- exercises/bloco_30/dia_3/exemplo_1_1/index.js
+++ exercises/bloco_30/dia_3/exemplo_1_1/index.js
@@ -1,8 +1,27 @@
 // index.js
 const express = require('express');
-const { Address, Employee } = require('./models');
+const { Address, Employee, Book, User } = require('./models');
 
 const app = express();
+
+// Exemplo para testar relacionamento N:N
+app.get('/usersbooks/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findOne({
+        where: { userId: id },
+        include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+      });
+  
+      if (!user)
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+  
+      return res.status(200).json(user);
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    };
+  });
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -15,6 +34,35 @@ app.get('/employees', async (_req, res) => {
     console.log(e.message);
     res.status(500).json({ message: 'Ocorreu um erro' });
   };
+});
+
+app.get('/employees/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const employee = await Employee.findOne({Where:{id}});
+
+      if(!employee) return res.status(404).json({message:'Funcionário não encontrado'});
+
+      if(req.query.includeAdresses === 'true'){
+          const addresses = await Address.findAll({where:{employeeId:id}});
+          return res.status(200).json({employee, addresses});
+      }
+      /*
+      const employee = await Employee.findOne({
+          where: { id },
+       // include: [{ model: Address, as: 'addresses' }],
+       // abaixo a propriedade number não será exibida
+          include: [{model: Address, as: 'addresses', attributes: { exclude: ['number'] },}],
+        });
+        
+      if (!employee)
+        return res.status(404).json({ message: 'Funcionário não encontrado' });
+     */
+      return res.status(200).json(employee);
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    };
 });
 
 const PORT = process.env.PORT || 3000;
