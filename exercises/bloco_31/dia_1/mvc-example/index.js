b9f3bcdc63894efcfcc9bde0d24c73197c14caf0
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-10-20 19:56:24 -0300

    MVC - exemplo inserindo novos dados

--- exercises/bloco_31/dia_1/mvc-example/index.js
+++ exercises/bloco_31/dia_1/mvc-example/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const authorController = require('./controllers/authorController');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); //configura o express para utilizar o EJS por padrão como template engine . Dessa forma, 
 // não precisamos especificar a extensão do arquivo que queremos utilizar.
@@ -12,8 +15,12 @@ app.set('views', './views'); //adiciona o diretório /views à lista de diretór
 
 app.get('/authors', authorController.listAuthors);
 
+app.get('/authors/new', authorController.newAuthor); // esse endpoint obrigatoriamente vai primeiro que o endpoint post para nao dar 404
+
 // exemplo pagina de detalhes de um escritor 
 app.get('/authors/:id', authorController.showAuthor);
+
+app.post('/authors', authorController.createAuthor);
 
 const PORT = process.env.PORT || 3000;
 
