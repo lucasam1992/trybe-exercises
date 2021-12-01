b90055b0684a02ff0163e3b3f551e3cd4204636c
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-11-30 22:04:58 -0300

    Mais funçoes em python

--- exercises/bloco_32/dia_1/exemplos_aula.py
+++ exercises/bloco_32/dia_1/exemplos_aula.py
@@ -1,3 +1,5 @@
+import math
+
 2 * 3  # saída: 6
 2 + 3  # saída: 5
 3 / 2  # saída: 1.5
@@ -184,3 +186,60 @@ while last < n:
     last, next = next, last + next
 
 # Funções
+
+
+def soma(x, y):
+    return x + y
+
+    soma(2, 2)  # os parâmetros aqui são posicionais
+
+    soma(x=2, y=2)  # aqui estamos nomeando os parâmetros
+
+# Os parâmetros também podem ser variádicos. Ou seja, podem variar em sua
+#  quantidade. Parâmetros posicionais variádicos são acessados como
+#  tuplas no interior de uma função e parâmetros nomeados variádicos como
+#  dicionário.
+
+
+def concat(*strings):
+    # Equivalente a um ", ".join(strings), que concatena os elementos de um
+    #  iterável em uma string utilizando um separador
+    # Nesse caso a string resultante estaria separada por vírgula
+    final_string = ""
+    for string in strings:
+        final_string += string
+        if not string == strings[-1]:
+            final_string += ', '
+    return final_string
+
+# pode ser chamado com 2 parâmetros
+    concat("Carlos", "João")  # saída: "Carlos, João"
+
+# pode ser chamado com um número n de parâmetros
+    concat("Carlos", "João", "Maria")  # saída: "Carlos, João, Maria"
+
+# dict é uma função que já vem embutida no python
+    dict(nome="Felipe", sobrenome="Silva", idade=25)  # cria um dicionário
+    # utilizando as chaves passadas
+
+    dict(nome="Ana", sobrenome="Souza", idade=21, turma=1)  # o número de
+    # parâmetros passados para a função pode variar
+
+
+# len([1, 2, 3, 4])  # função len não aceita argumentos nomeados
+
+# len(obj=[1, 2, 3, 4])  # este código irá falhar
+
+print("Botaro", "Cássio", ", ")  # imprime Botaro Cássio ,
+
+print("Botaro", "Cássio", sep=", ")  # nomeando o terceiro parâmetro, agora
+# temos a saída: Botaro, Cássio
+
+# Clausula help
+help("if")
+
+help(abs)
+
+help(len)
+
+help(math.sin)
