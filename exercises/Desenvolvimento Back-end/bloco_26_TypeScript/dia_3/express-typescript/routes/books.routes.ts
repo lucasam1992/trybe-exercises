6277b9a746463af9a9b733a973befb739e2caf9e
lucasam1992 <lucas.almeida.machado92@gmail.com>
2023-04-06 21:49:38 -0300

    books findAll endpoint

--- exercises/Desenvolvimento Back-end/bloco_26_TypeScript/dia_3/express-typescript/routes/books.routes.ts
+++ exercises/Desenvolvimento Back-end/bloco_26_TypeScript/dia_3/express-typescript/routes/books.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import BooksController from '../controllers/books.controller';
+
 const router = Router();
+
+const booksController = new BooksController();
+
+router.get('/books', booksController.getAll);
 
 export default router;
