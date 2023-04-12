e71382678e2f197a2b5ef0b963f667d24129fa08
lucasam1992 <lucas.almeida.machado92@gmail.com>
2023-04-12 12:17:21 -0300

    Exemplos POO

---
+++ exercises/Desenvolvimento Back-end/Programação_Orientada_a_Objeto_e_SOLID/dia_1/classTV.ts
@@ -0,0 +1,21 @@
+class TV {
+    brand: string;
+    size: number;
+    resolution: string;
+    connections: string[];
+    connectedTo: string;
+
+    constructor(b: string, s: number, r: string, c: string[]) {
+        this.brand = b;
+        this.size = s;
+        this.resolution = r;
+        this.connections = c;
+    }
+
+    turnOn(): void {
+        console.log(`${this.brand, this.size, this.resolution, this.connections}`);
+    }
+}
+
+const tv1 = new TV('Sansung', 48, 'FullHD', ['VGA', 'HDMI', 'Wi-Fi']);
+tv1.turnOn();
