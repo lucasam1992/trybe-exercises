24ac24f0abe2e248b4fd4c1c65749327c913833d
lucasam1992 <lucas.almeida.machado92@gmail.com>
2023-06-29 12:04:28 -0300

    authentication jwt

--- exercises/Desenvolvimento Back-end/bloco_26_TypeScript/dia_3/API REST com Express, JWT, Sequelize e TypesCript/transactions-api/src/app.ts
+++ exercises/Desenvolvimento Back-end/bloco_26_TypeScript/dia_3/API REST com Express, JWT, Sequelize e TypesCript/transactions-api/src/app.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
+import authMiddleware from './middlewares/auth.middleware';
+import loginRouter from './routers/login.router';
 import transactionsRouter from './routers/transactions.router';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(loginRouter);
+app.use(authMiddleware);
 app.use(transactionsRouter);
 
 app.get('/', (_req: Request, res: Response) => {
